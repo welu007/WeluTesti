edf27d324f607c273a6fedbbd2416d50f28578fb
Sacrejaune <deicida@netti.fi>
2016-03-01 18:34:12 +0200

    Initial commit
    Initial commit

---
+++ Assets/BounceSound.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BounceSound : MonoBehaviour {
+	public AudioSource audio;
+
+	void OnCollisionEnter(Collision collision) {
+		foreach (ContactPoint contact in collision.contacts) {
+			Debug.DrawRay(contact.point, contact.normal, Color.white);
+		}
+
+		//if (collision.relativeVelocity.magnitude > 2)
+			audio.Play(); 
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
